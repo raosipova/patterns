55f4b23bf09bb9359bac79e856e7aac8d41408ae
Regina <OsRigli87@mail.ru>
2024-05-10 11:25:27 +0500

    Allure

--- src/test/java/ru/netology/DeliveryTest.java
+++ src/test/java/ru/netology/DeliveryTest.java
@@ -1,8 +1,8 @@
 package ru.netology;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
@@ -11,9 +11,15 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-
 class CardDeliveryTest {
 
+    @BeforeAll
+    static void setUPAll(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+    @AfterAll
+    static void tearDownAll() {
+    }
     @BeforeEach
     void setup() {
         open("http://localhost:9999");
